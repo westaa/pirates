0fb0a499636d68ee67f2c6d41a2700387e82537a
Andrew West <andrewwest@Andrews-MacBook-Pro.local>
2016-08-07 17:04:43 -0600

    instantiated pirates module

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('pirates', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/pirates',{
+      templateUrl: "/views/pirates.html",
+      controller: "piratesController"
+    })
+})
---
+++ public/javascripts/controllers/controller.js
@@ -0,0 +1,4 @@
+app.controller('piratesController', function($scope){
+  $scope.view = {};
+  $scope.view.greeting = "Yargh, 'tis a fine day for pillage and plunder!'"
+});
